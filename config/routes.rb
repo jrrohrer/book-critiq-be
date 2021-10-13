0bef90345a48d739bdd591e006190a034fc34479
jrrohrer <jessie.rohrer@gmail.com>
2021-10-13 15:21:33 -0400

    namespace book routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :books
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :books
     end
   end
 end
