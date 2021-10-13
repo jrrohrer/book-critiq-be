283853a9d210ae3d41564999db0b1f865b1a024d
jrrohrer <jessie.rohrer@gmail.com>
2021-10-13 15:20:57 -0400

    scaffold book model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
