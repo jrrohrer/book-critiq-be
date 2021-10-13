cd5b2db546b6e46c719571d366776599bfd77835
jrrohrer <jessie.rohrer@gmail.com>
2021-10-13 16:01:13 -0400

    scaffold comment model/controller, namespaced routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   has_many :reviews
   has_many :books, through: :reviews
-  #has_many :comments
-  #has_many :reviews, through: :comments
+  has_many :comments
+  has_many :reviews, through: :comments
 end
