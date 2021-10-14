d4feb6842fa7216740b8f22a922709ab0bce71df
jrrohrer <jessie.rohrer@gmail.com>
2021-10-14 14:01:15 -0400

    add image url to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_193616) do
+ActiveRecord::Schema.define(version: 2021_10_14_173953) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_13_193616) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "comments", force: :cascade do |t|
