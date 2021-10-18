244457708ac70a399a098e7550556db94193cc4f
jrrohrer <jessie.rohrer@gmail.com>
2021-10-18 17:28:54 -0400

    nest review routes under book routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :books
-      resources :reviews
+      resources :books do
+        resources :reviews
+      end
       resources :comments
     end
   end
