e2918d091a4bac2eaff59896e7c3f533c7fafba0
jrrohrer <jessie.rohrer@gmail.com>
2021-10-21 17:45:16 -0400

    add validations to Book and Review models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   has_many :reviews
+
+  validates :title, :author, :description, :image_url, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   belongs_to :book
   # has_many :comments
   # has_many :users, through: :comments
+
+  validates :title, :content, presence: true
 end
