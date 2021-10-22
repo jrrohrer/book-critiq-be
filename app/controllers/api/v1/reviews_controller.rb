4dcef2fd7030fa67687534abc2bd49167583ebbd
jrrohrer <jessie.rohrer@gmail.com>
2021-10-21 22:33:43 -0400

    comment out currently unused controller actions

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :update, :destroy]
 
   # GET /reviews
-  def index
-    @reviews = Review.all
+  # def index
+  #   @reviews = Review.all
 
-    render json: @reviews
-  end
+  #   render json: @reviews
+  # end
 
   # GET /reviews/1
-  def show
-    render json: @review
-  end
+  # def show
+  #   render json: @review
+  # end
 
   # POST /reviews
   def create
@@ -25,13 +25,13 @@ class Api::V1::ReviewsController < ApplicationController
   end
 
   # PATCH/PUT /reviews/1
-  def update
-    if @review.update(review_params)
-      render json: @review
-    else
-      render json: @review.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @review.update(review_params)
+  #     render json: @review
+  #   else
+  #     render json: @review.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /reviews/1
   def destroy
